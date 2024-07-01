b28a9f0f6191f5af4d9d170e0801c65fcf96e3a3
Leonardo Lima <leonardo@paguextech.com>
2024-07-01 13:16:46 -0300

    ajuste protected route

--- src/Routes/ useProviderType.routes.tsx
+++ src/Routes/ useProviderType.routes.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { ProviderType } from "../Components/Pages/ ProviderType";
+import { ProviderType } from "../Components/Pages/ProviderType";
+import { ProtectedRoute } from "../Components/Atoms/ProtectedRoute";
 
 export const useProviderTypeRoutes = () => {
-  return <Route path="/tipo-prestadores" element={<ProviderType />} />;
+  return (
+    <Route
+      path="/tipo-prestadores"
+      element={
+        <ProtectedRoute>
+          <ProviderType />
+        </ProtectedRoute>
+      }
+    />
+  );
 };
--- src/Routes/useProfessionals.routes.tsx
+++ src/Routes/useProfessionals.routes.tsx
@@ -4,15 +4,44 @@ import { Professionals } from "../Components/Pages/Professionals";
 import { ProfessionalRegister } from "../Components/Pages/ProfessionalRegister";
 import { ProfessionalRegisterDetail } from "../Components/Pages/ProfessionalRegisterDetail";
 import { ProfessionalSchedule } from "../Components/Pages/ProfessionalSchedule";
+import { ProtectedRoute } from "../Components/Atoms/ProtectedRoute";
 
 export const userProfessionalsRoutes = () => {
   return (
     <Route path="/profissionais">
-      <Route index element={<Professionals />} />
+      <Route
+        index
+        element={
+          <ProtectedRoute>
+            <Professionals />
+          </ProtectedRoute>
+        }
+      />
       <Route path="cadastro">
-        <Route index element={<ProfessionalRegister />} />
-        <Route path="detalhe" element={<ProfessionalRegisterDetail />} />
-        <Route path="agenda" element={<ProfessionalSchedule />} />
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <ProfessionalRegister />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="detalhe"
+          element={
+            <ProtectedRoute>
+              <ProfessionalRegisterDetail />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="agenda"
+          element={
+            <ProtectedRoute>
+              <ProfessionalSchedule />
+            </ProtectedRoute>
+          }
+        />
       </Route>
     </Route>
   );
