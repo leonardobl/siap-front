cb1bf9c574bc3b49879eba9964f7446d66986091
Leonardo Lima <leonardo@paguextech.com>
2024-07-11 14:27:22 -0300

    merge conflito

--- cypress/e2e/Login.spec.cy.tsx
+++ cypress/e2e/Login.spec.cy.tsx
@@ -5,7 +5,7 @@ export const DATA_USER_ADMIN = {
   password: "STARcheck#123",
 };
 
-const DATA_USER_CLIENT = {
+export const DATA_USER_CLIENT = {
   login: "118.724.500-32",
   password: "1234",
 };
