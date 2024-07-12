a33ea962b752fc2d5ae40ade36df8d63fa153a5a
Leonardo Lima <leonardo@paguextech.com>
2024-07-12 12:36:46 -0300

    testes para ciracao do ususario

--- cypress/e2e/Clientes/Clientes.spec.cy.tsx
+++ cypress/e2e/Clientes/Clientes.spec.cy.tsx
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
-import { DATA_USER_ADMIN } from "../../fixtures/DataUser";
+import { maskCep } from "../../../src/Utils/masks";
+import { DATA_CLIENT, DATA_USER_ADMIN } from "../../fixtures/DataUser";
 
 describe("Clientes", () => {
   beforeEach(() => {
@@ -54,5 +55,38 @@ describe("Clientes", () => {
           .should("contain.text", body.content[0].nome);
       }
     });
+  });
+
+  it("Deve cadastrar um cliente com sucesso", () => {
+    cy.get("button").contains("Cadastrar").click();
+
+    cy.location("pathname").should("eq", "/clientes/cadastro");
+
+    cy.get("input[id='nome']").type(DATA_CLIENT.nome);
+    cy.get("input[id='cpf']").type(DATA_CLIENT.cpf);
+    cy.get("input[id='telefone']").type(DATA_CLIENT.telefone);
+    cy.get("input[id='email']").type(DATA_CLIENT.email);
+    cy.get("input[id='cep']").type(DATA_CLIENT.cep).as("buttonCep");
+    cy.get("@buttonCep").blur();
+    cy.intercept({
+      method: "GET",
+      url: `https://viacep.com.br/ws/${maskCep(DATA_CLIENT.cep)}/json`,
+    }).as("getCep");
+
+    cy.wait("@getCep").then(() => {
+      cy.get("input[id='numero']").type(DATA_CLIENT.numero);
+      cy.get("input[id='senha']").type(DATA_CLIENT.senha);
+      cy.get("input[id='confirm']").type(DATA_CLIENT.senha);
+      cy.get("form").submit();
+    });
+
+    cy.intercept({
+      method: "POST",
+      url: `https://agendamentos-api-staging.siap.tec.br:8443/cliente/cadastrar`,
+    }).as("submitForm");
+
+    cy.wait("@submitForm")
+      .then(({ response }) => response.statusCode)
+      .should("eq", 201);
   });
 });
--- cypress/fixtures/DataUser.ts
+++ cypress/fixtures/DataUser.ts
@@ -1,3 +1,5 @@
+import { geradorDeCpf } from "../../src/Utils/geradorDeCpf";
+const timeStamp = new Date().getTime();
 export const DATA_USER_ADMIN = {
   login: "014.269.043-04",
   password: "STARcheck#123",
@@ -6,4 +8,14 @@ export const DATA_USER_ADMIN = {
 export const DATA_USER_CLIENT = {
   login: "118.724.500-32",
   password: "1234",
+};
+
+export const DATA_CLIENT = {
+  nome: `Usuario de teste ${timeStamp}`,
+  cpf: geradorDeCpf(),
+  telefone: "869999999999",
+  email: `${timeStamp}@teste.com.br`,
+  cep: "64049700",
+  numero: "2020",
+  senha: "1234",
 };
