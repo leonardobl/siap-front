95f538e71249f243aa1e9a65373d259a1e60aaf0
Leonardo Lima <leonardo@paguextech.com>
2024-07-12 08:26:32 -0300

    criacao do teste de listagem de clientes

--- cypress/e2e/Clientes/Clientes.spec.cy.tsx
+++ cypress/e2e/Clientes/Clientes.spec.cy.tsx
@@ -17,18 +17,18 @@ describe("Clientes", () => {
 
   it.only("Deve exibir a listagem de clientes", () => {
     cy.goToByTitle("Clientes");
-    cy.get("div[data-cy='clients-wraper']").should("be.visible");
 
     cy.intercept({
       method: "GET",
-      url: "https:agendamentos-api-staging.siap.tec.br:8443/cliente/listar?size=7&page=0",
+      url: "https://agendamentos-api-staging.siap.tec.br:8443/cliente/listar?size=7&page=0",
     }).as("getClientes");
 
+    cy.get("div[data-cy='clients-wrapper']").should("be.visible");
+
     cy.wait("@getClientes").then(({ response: { body } }) => {
-      cy.get("div[data-cy='clients-list']").should(
-        "have.length",
-        body.content.length
-      );
+      cy.get("div[data-cy='clients-list']")
+        .children()
+        .should("have.length", body.content.length);
     });
   });
 });
